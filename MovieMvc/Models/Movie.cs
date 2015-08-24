6bb111492d33d39c9a92d9ba4024ecb0fc680852
armory09 <xianlee.ivan.guinto@gmail.com>
2015-08-24 10:14:09 +0800

    adding validation

--- MovieMvc/Models/Movie.cs
+++ MovieMvc/Models/Movie.cs
@@ -6,14 +6,26 @@ namespace MovieMvc.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$",ErrorMessage = "Genre Field Must be A-Z")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1,100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$",ErrorMessage = "Rating Field Must be A-Z")]
+        [StringLength(5)]
         public string Rating { get; set; }
 
     }
