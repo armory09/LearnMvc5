87e99cb84045834436effd0cbb8c7d706df72194
armory09 <xianlee.ivan.guinto@gmail.com>
2015-08-24 09:24:23 +0800

    Add Rating Field

--- MovieMvc/Controllers/MoviesController.cs
+++ MovieMvc/Controllers/MoviesController.cs
@@ -64,7 +64,7 @@ namespace MovieMvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Create([Bind(Include = "Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -96,7 +96,7 @@ namespace MovieMvc.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
